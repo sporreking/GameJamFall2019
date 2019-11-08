718d85880e4634b59c23367f6ed9265b5b739110
Robert Sehlstedt <robert.sehlstedt94@gmail.com>
2019-11-08 23:10:27 +0100

    Player can move with wasd or controller, also some building assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+
+    public float Speed;
+    public float RotateSpeed;
+
+    private CharacterController Controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate around y - axis
+        transform.Rotate(0, Input.GetAxis("Horizontal") * RotateSpeed, 0);
+
+        // Move forward / backward
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = Speed * Input.GetAxis("Vertical");
+        Controller.SimpleMove(forward * curSpeed);
+    }
+}
