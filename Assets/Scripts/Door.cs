21f180aef1b689e06339c20b3de7b85d1bbb7449
Alfred Sporre <alfred@familjensporre.se>
2019-11-08 23:14:30 +0100

    intermediate

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public bool Locked;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (Locked)
+            Lock() ;
+    }
+
+
+    public void Unlock()
+    {
+        Locked = false;
+        rb.freezeRotation = false;
+    }
+
+    public void Lock()
+    {
+        rb.freezeRotation = true;
+    }
+}
---
+++ Assets/Scripts/KeyResolver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyResolver : MonoBehaviour
+{
+    public List<GameObject> Keys;
+
+    public UnityEvent OnKeyResolve;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Keys.Contains(collision.gameObject))
+        {
+            OnKeyResolve.Invoke();
+        }
+    }
+}
