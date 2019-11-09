639f7c85ceebc22ac3986a68716c1749bfe058d5
Alfred Sporre <alfred@familjensporre.se>
2019-11-10 00:50:38 +0100

    Added chaos factor to player

---
+++ Assets/Scripts/HurtPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPoint : MonoBehaviour
+{
+    public float HurtRadius;
+
+    private GameObject p;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 d = transform.position - p.transform.position;
+
+        if (d.magnitude < HurtRadius)
+        {
+            p.GetComponent<Player>().SetChaosFactor(1.0f - d.magnitude / HurtRadius);
+        }
+    }
+}
