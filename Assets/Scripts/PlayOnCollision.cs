00fab434ae8bfb5a107ce882ec0384b0e6f9de48
Robert Sehlstedt <robert.sehlstedt94@gmail.com>
2019-11-10 14:43:27 +0100

    Adds audio trigger area

---
+++ Assets/Scripts/PlayOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnCollision : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
